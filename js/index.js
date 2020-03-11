c9c8973950a1deb2378acf79059390df0b86b184
JoelTi <61313145+JoelTi@users.noreply.github.com>
2020-03-11 14:53:22 +0100

    animaties & weerbericht
    animaties toegevoegd en het weerbericht past zich nu aan aan de tijd

--- js/index.js
+++ js/index.js
@@ -16,22 +16,46 @@ function showTime() {
   s = s < 10 ? "0" + s : s;
   // var time = h + ":" + m + ":" + s + " " + session;
   var time = h + ":" + m + ":" + s;
-  document.getElementById("MyClockDisplay").innerText = time;
-  document.getElementById("MyClockDisplay").textContent = time;
+  document.getElementById("myClock").innerText = time;
+  document.getElementById("myClock").textContent = time;
   setTimeout(showTime, 1000);
+}
+
+function showWeather() {
+  var now = new Date();
+  var hour = now.getHours();
+
+  if (hour >= 6 && hour < 12) {
+    document.getElementById("weatherbox-one").classList.add('box-shadow');
+    document.getElementById("btn-weather1").classList.add('weather-button');
   }
+  else if (hour >= 12 && hour < 18) {
+    document.getElementById("weatherbox-two").classList.add('box-shadow');
+    document.getElementById("btn-weather2").classList.add('weather-button');
+  }
+  else if (hour >= 18 && hour < 24) {
+    document.getElementById("weatherbox-three").classList.add('box-shadow');
+    document.getElementById("btn-weather3").classList.add('weather-button');
+  }
+  else {
+    document.getElementById("weatherbox-four").classList.add('box-shadow');
+    document.getElementById("btn-weather4").classList.add('weather-button');
+  }
+}
 
 function showNav() {
   var x = document.getElementById('menu-mobile');
   if (x.style.display === 'none') {
     x.style.display = 'block';
-  } else {
+  }
+  else {
     x.style.display = 'none';
   }
 }
 
 window.onload = function() {
   showTime();
+  showWeather();
 }
 // document.getElementsByClassName('div button').onclick = function(){
 //   showNav();
