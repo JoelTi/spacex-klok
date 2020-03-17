5c5e470295f2d2b6ebc7f539ce02d9571e8e5948
JoelTi <61313145+JoelTi@users.noreply.github.com>
2020-03-17 10:01:26 +0100

    /

--- js/index.js
+++ js/index.js
@@ -3,14 +3,6 @@ function showTime() {
   var h = date.getHours(); // 0 - 23
   var m = date.getMinutes(); // 0 - 59
   var s = date.getSeconds(); // 0 - 59
-  // var session = "AM";
-  // if (h == 0) {
-  //   h = 12;
-  // }
-  // if (h > 12) {
-  //   h = h - 12;
-  //   session = "PM";
-  // }
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
@@ -57,6 +49,3 @@ window.onload = function() {
   showTime();
   showWeather();
 }
-// document.getElementsByClassName('div button').onclick = function(){
-//   showNav();
-// }
